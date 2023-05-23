045824936beffbebd2bc2da74b17f7717cc9a16d
YALIM GÜRBÜZ <yalim.gurbuz@std.yildiz.edu.tr>
2023-05-23 15:44:46 +0300

    CRUDWebAPI-with-EF-Core-Repository-Pattern

---
+++ CRUDWebAPI/Data/ApiDbContext.cs
@@ -0,0 +1,11 @@
+﻿using CRUDWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDWebAPI.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options) { }
+        public DbSet<Driver> Drivers { get; set; }
+    }
+}
